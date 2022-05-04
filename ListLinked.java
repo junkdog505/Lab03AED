3b9928cd1d0b9edeff2d0fcb76193a42db7d8449
junkdog505 <junkdog505@gmail.com>
2022-05-04 08:43:14 -0500

    Se añadió el método remove

--- ListLinked.java
+++ ListLinked.java
@@ -7,7 +7,17 @@ public class ListLinked<E> implements TDAList<E>{
     public boolean search(E x){return true;}
     public void insertFirst(E x) {this.first = new Node <E>(x, this.first);}
     public void insertLast(E x) {}
-    public void remove(E x) {}
+    public void remove(E x) {
+        if(this.first.getData().equals(x))   //eliminación del primer nodo.
+            this.first = this.first.getNext();
+        else{
+            Node<E> aux = this.first;
+            while(aux.getNext() != null && !aux.getNext().getData().equals(x))
+                aux = aux.getNext();
+            if(aux.getNext() != null)
+                aux.setNext(aux.getNext().getNext());
+        }
+    }
     public String toString() {
         String str = "";
         for(Node<E> aux = this.first; aux != null; aux = aux.getNext()){
--- Test.java
+++ Test.java
@@ -3,17 +3,27 @@ public class Test {
         TDAList<Integer> intList = new ListLinked<Integer>();
 
         intList.insertFirst(20);
+        System.out.println(intList);
         intList.insertFirst(10);
+        System.out.println(intList);
         intList.insertFirst(30);
+        System.out.println(intList);
         intList.insertFirst(26);
+        System.out.println(intList);
         intList.insertFirst(7);
         System.out.println(intList);
+        intList.remove(7);
+        System.out.println(intList);
+        intList.remove(30);
+        System.out.println(intList);
+        intList.remove(20);
+        System.out.println(intList);
 
-        TDAList<String> stringList = new ListLinked<String>();
+        /* TDAList<String> stringList = new ListLinked<String>();
         stringList.insertFirst("Cristian");
         stringList.insertFirst("Amezquita");
         stringList.insertFirst("Edison");
-        stringList.insertFirst("Soto");
-        System.out.println(stringList);
+        stringList.insertFirst("");
+        System.out.println(stringList); */
     }
 }
